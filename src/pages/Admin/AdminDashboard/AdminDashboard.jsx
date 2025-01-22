e18e07e6e570d8db60c7de3be8fbc2c248dadc4b
SkrypaD <skrypchenkodanil@gmail.com>
2025-01-22 23:00:08 +0200

    Pages initialized. Home and movies page initialy implemented.

---
+++ src/pages/Admin/AdminDashboard/AdminDashboard.jsx
@@ -0,0 +1,9 @@
+import styles from "./styles/AdminDashboard.module.css"
+
+function AdminDashboard() {
+    return (
+        <h1> AdminDashboard </h1>
+    )
+}
+
+export default AdminDashboard
---
+++ src/pages/Common/SignUpPage/SignUpPage.jsx
@@ -0,0 +1,9 @@
+import styles from "./styles/SignUpPage.module.css"
+
+function SignUpPage() {
+    return (
+        <h1> SignUpPage </h1>
+    )
+}
+
+export default SignUpPage
